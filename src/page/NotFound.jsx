d676b2d3a76c9eeea219f56acdd0a2c1c96af3bf
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-11-21 14:04:55 +0200

    update

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { StyledContainer } from './NotFound.styled';
 import notFound from '../image/kisspng.png';
+import { BiLeftArrow } from 'react-icons/bi';
 const NotFound = () => {
   const location = useLocation();
   const goBackRef = useRef(location.state?.from || '/');
@@ -9,8 +10,12 @@ const NotFound = () => {
   return (
     <StyledContainer>
       <p>
-        Movie not found <br />
-        <Link to={goBackRef.current}> Go Back</Link>
+        <Link to={goBackRef.current}>
+          <BiLeftArrow />
+          Go back
+        </Link>
+        <br />
+        Sorry, but the movie was not found. Go back or return to the main page.{' '}
       </p>
       <img src={notFound} alt="404" width={200} height={200} />
     </StyledContainer>
--- src/page/NotFound.styled.js
+++ src/page/NotFound.styled.js
@@ -5,20 +5,38 @@ export const StyledContainer = styled.div`
   padding: 5px;
   border-radius: 10px;
   padding-top: 50px;
-  & img {
+  max-width: 1000px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  img {
     width: 500px;
-    margin-left: 50px;
   }
-  & p {
-    margin-left: 250px;
-    & a {
-      font-size: 30px;
-      font-weight: bold;
-      text-transform: uppercase;
-      color: black;
+  p {
+    margin-bottom: 10px;
+    a {
       transition: all 0.3s ease;
+      display: inline-flex;
+      color: black;
+      font-size: 14px;
+      padding: 3px 10px;
+      border: 1px solid black;
+      border-radius: 5px;
+      margin-bottom: 10px;
+      cursor: pointer;
+      text-decoration: none;
+      align-items: center;
+      &:focus,
       &:hover {
-        color: blue;
+        color: white;
+        text-shadow: 0px 0px 5px white;
+      }
+      &:focus svg,
+      &:hover svg {
+        /* box-shadow: 0px 0px 5px 1px white; */
+        filter: drop-shadow(0px 0px 5px white);
       }
     }
   }
